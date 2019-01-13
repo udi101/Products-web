0a3c651eb0d8b740f359f1b4c31ae5b78f02281f
Udi Mazor <udi.mazor@graduway.com>
2019-01-13 14:30:17 +0200

    create Common.models

--- products/App_Start/WebApiConfig.cs
+++ products/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace products
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
---
+++ products/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Common.Models;
+
+namespace products.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Products/5
+        public Product Get(int id)
+        {
+            var result = new Product() { ProductId = 3 };
+            return result;
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
